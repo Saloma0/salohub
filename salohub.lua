9864c40b47250a4b6ef2a3aba15f027e71020c1e
Salomao <167381996+Saloma0@users.noreply.github.com>
2025-07-22 14:17:29 -0300

    Update salohub.lua

--- salohub.lua
+++ salohub.lua
@@ -1,10 +1,11 @@
 local Scripts = {
-	{
-		PlacesIds = {286090429},
-		UrlPath = 'loadstring(game:HttpGet("https://raw.githubusercontent.com/Saloma0/SaloHub-Arsenal/refs/heads/main/salohub.lua"))()'
-	},
-	{
-		PlacesIds = {4924922222},
-		UrlPath = 'loadstring(game:HttpGet("https://raw.githubusercontent.com/Saloma0/SaloHubBrookhaven/refs/heads/main/SaloHub"))()'
-	}
+    [286090429] = 'https://raw.githubusercontent.com/Saloma0/SaloHub-Arsenal/refs/heads/main/salohub.lua',
+    [4924922222] = 'https://raw.githubusercontent.com/Saloma0/SaloHubBrookhaven/refs/heads/main/SaloHub'
 }
+
+local scriptUrl = Scripts[game.PlaceId]
+if scriptUrl then
+    loadstring(game:HttpGet(scriptUrl))()
+else
+    warn("Nenhum script configurado para este PlaceId:", game.PlaceId)
+end
